64cf2a1bcb2a669d073db94e21c5de3b7b1044b4
Max Ralph <maxralph01@gmail.com>
2023-03-17 19:43:22 +0400

    completed tests and added api documentation

--- tests/Feature/ParkingTest.php
+++ tests/Feature/ParkingTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Zone;
+use App\Models\Parking;
 use App\Models\Vehicle;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -50,5 +51,58 @@ class ParkingTest extends TestCase
     {
         $user = User::factory()->create();
         $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+        $zone = Zone::first();
+
+        $this->actingAs($user)->postJson('/api/v1/parkings/start', [
+            'vehicle_id' => $vehicle->id,
+            'zone_id' => $zone->id
+        ]);
+
+        $this->travel(2)->hours();
+
+        $parking = Parking::first();
+        $response = $this->actingAs($user)->getJson('/api/v1/parkings/' . $parking->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJson([
+                'data' => [
+                    'stop_time' => null,
+                    'total_price' => $zone->price_per_hour * 2
+                ]
+            ]);
+    }
+
+    public function testUserCanStopParking()
+    {
+        $user = User::factory()->create();
+        $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+        $zone = Zone::first();
+
+        $this->actingAs($user)->postJson('/api/v1/parkings/start', [
+            'vehicle_id' => $vehicle->id,
+            'zone_id' => $zone->id
+        ]);
+
+        $this->travel(2)->hours();
+
+        $parking = Parking::first();
+        $response = $this->actingAs($user)->putJson('/api/v1/parkings/' . $parking->id);
+
+        $updatedParking = Parking::find($parking->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data'])
+            ->assertJson([
+                'data' => [
+                    'start_time' => $updatedParking->start_time->toDateTimeString(),
+                    // 'start_time'  => $updatedParking->start_time->format('Y-m-d\TH:i:s.u\Z'),
+                    'stop_time' => $updatedParking->stop_time->toDateTImeString(),
+                    // 'stop_time'   => $updatedParking->stop_time->format('Y-m-d\TH:i:s.u\Z'),
+                    'total_price' => $updatedParking->total_price
+                ]
+            ]);
+
+        $this->assertDatabaseCount('parkings', '1');
     }
 }
